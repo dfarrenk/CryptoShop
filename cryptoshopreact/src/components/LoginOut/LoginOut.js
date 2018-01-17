989f995aed01a5461d0649e0d2b676d338a6707a
71emj <tim.jeng@gmail.com>
2018-01-17 16:04:02 -0500

    basic login ui with react

---
+++ cryptoshopreact/src/components/LoginOut/LoginOut.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Login extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: null,
+			password: null,
+			passconfirm: null,
+			email: null,
+			isLogin: true
+		};
+	}
+
+	inputHandler = evt => {
+		let { name, value } = evt.target;
+
+		// handler to identify user name or email
+
+		this.setState({
+			[name]: value
+		});
+	};
+
+	submitHandler = evt => {
+		evt.preventDefault();
+		console.log(this.state);
+	}
+
+	renFooter() {
+		const { isLogin } = this.state;
+		const msg = isLogin ? "New User?" : "Already Registered?";
+
+		return <a onClick={() => this.setState({ isLogin: !isLogin })}>{msg}</a>;
+	}
+
+	renLogin() {
+		const fields = [
+			{
+				name: "username",
+				val: "Username or Email"
+			},
+			{
+				name: "password",
+				val: "Password",
+				type: "password"
+			}
+		];
+
+		return this.renderFields(fields);
+	}
+
+	renRegister() {
+		const fields = [
+			{
+				name: "username",
+				val: "Username"
+			},
+			{
+				name: "email",
+				val: "Email Address",
+				type: "email"
+			},
+			{
+				name: "password",
+				val: "Password",
+				type: "password"
+			},
+			{
+				name: "passwordconfirm",
+				val: "Confirm Your Password",
+				type: "password"
+			}
+		];
+
+		return this.renderFields(fields);
+	}
+
+	renderFields(args) {
+		const fields = args.map((elem, index) => {
+			return (
+				<div key={index} className="form-group">
+					<label htmlFor={elem.name}>
+						{elem.name.replace(/\b\w/g, elem => elem.toUpperCase()) + ": "}
+					</label>
+					<input
+						type={elem.type ? elem.type : "text"}
+						onChange={this.inputHandler}
+						name={elem.name}
+						id={elem.name}
+						placeholder={elem.val}
+						className="form-control"
+					/>
+				</div>
+			);
+		});
+
+		return fields;
+	}
+
+	render() {
+		const { isLogin } = this.state;
+
+		return (
+			<form onSubmit={this.submitHandler} className="card">
+				{isLogin ? this.renLogin() : this.renRegister()}
+				<input type="submit" value={isLogin ? "Login" : "Register"} /> 
+				{this.renFooter()}
+			</form>
+		);
+	}
+}
+
+export default Login;
