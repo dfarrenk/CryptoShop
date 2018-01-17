ee7b38af9a12ac04edac3f6039caa07aaf70a7ed
71emj <tim.jeng@gmail.com>
2018-01-17 00:31:59 -0500

    session base authentication + timeout

--- config/jwt_config.js
+++ config/jwt_config.js
@@ -2,12 +2,22 @@ const PassportJwt = require("passport-jwt");
 const ExtractJwt = PassportJwt.ExtractJwt;
 
 function ExtractFromCookie(req) {
-	let token = null;
-	if (req && req.cookies) {
-		token = req.cookies["jwt-token"];	
+	console.log(req.session);
+	const {authenticated, cookie} = req.session;
+	if (authenticated) {
+		return req.session.token;
 	}
-	return token;
+
+	return null;
 }
+
+// function ExtractFromCookie(req) {
+// 	let token = null;
+// 	if (req && req.cookies) {
+// 		token = req.cookies["jwt-token"];
+// 	}
+// 	return token;
+// }
 
 module.exports = {
 	secretOrKey: "some secret",
