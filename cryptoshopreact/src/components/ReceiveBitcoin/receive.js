eb5892e72826184f29e9f9286ccc84a65ea256ef
Vitaliy Vorobyev <vvitali26@gmail.com>
2018-01-18 18:43:09 -0500

    basic component

---
+++ cryptoshopreact/src/components/ReceiveBitcoin/receive.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class receiveModule extends Component{
+	render(){
+		return(
+			<div>Hello user name</div>
+			);		
+	}
+
+}
+export default receiveModule;
