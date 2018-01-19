212072d47b5aa4dbcff48f393c85b13643daffb2
Vitali Vorobyev <vvitali26@gmail.com>
2018-01-19 13:46:17 -0500

    temporary error handler for coinbase API

--- controllers/coinbase.js
+++ controllers/coinbase.js
@@ -1,26 +1,35 @@
-const someRoute = require("express").Router();
+const routes = require("express").Router();
 const client = require("../config/coinbase.js");
 console.log("Coinbase controller: \x1b[32mloaded!\x1b[0m");
-module.exports = function() {
 
-	client.getAccounts({}, function(err, accounts) {
-		if (err) throw err;
-		accounts.forEach(function(acct) {
-			console.log(acct.name + ': ' + acct.balance.amount + ' ' + acct.balance.currency);
-			acct.getTransactions(null, function(err, txns) {
-				txns.forEach(function(txn) {
-					console.log('txn: ' + txn.id);
-				});
-			});
-		});
+module.exports = function() {
+	routes.post("/getTransaction", (req, res)=>{
+		console.log("getTransaction route fires!");
+		res.send("Got it! Test")
 	});
 
+	client.getAccounts({}, function(err, accounts) {
+		//it looks strange, but we have to keep it before we coinbase will activate API-key
+		if (err){
+			console.error("Error:" + err.message);
+			err.message == "API Key disabled" && console.log("\x1b[32mEverything is fine, we have to wait 48 hours since 1/18/18 for API key activation\x1b[0m")
+		}else{
+			accounts.forEach(function(acct) {
+				console.log(acct.name + ': ' + acct.balance.amount + ' ' + acct.balance.currency);
+				acct.getTransactions(null, function(err, txns) {
+					txns.forEach(function(txn) {
+						console.log('txn: ' + txn.id);
+					});
+				});
+			});
+		}
+		
+	});
+
+	// this is not necessary for us
 	// client.createAccount({'name': 'New Wallet'}, function(err, acct) {
 	// 	console.log(acct.name + ': ' + acct.balance.amount + ' ' + acct.balance.currency);
 	// });
 
-
-
-
-	return someRoute;
+	return routes;
 }
