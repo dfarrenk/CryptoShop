ded69c0eafb48fbc61e0ab2452f4e219dcb45d08
Vitaliy Vorobyev <vvitali26@gmail.com>
2018-01-22 02:28:46 -0500

    0.2

--- controllers/eBay.js
+++ controllers/eBay.js
@@ -13,7 +13,8 @@ module.exports = function() {
 	*/
 	let options = {
 		headers: {
-			Authorization:"Bearer v^1.1#i^1#f^0#p^3#r^0#I^3#t^H4sIAAAAAAAAAOVXa2wUVRTu9mWaWk0TKaiAm0GNlszuvTv7mB3Y1e2DUgKlZUsFDJZ53GkHZmfGuTNtx2izKVL++Af8gcGgDQaE8lAQEwhKxEiQaMRHQomIolF8JIYIRhJE8c72wbZE6IPEJu6fyb33vL5zvnP2XpAuLCrvnt99ucRzR25PGqRzPR5YDIoKC2bflZd7X0EOyBLw9KQfTOd35f00F/Mp1eCWIGzoGkbejpSqYS6zGaNsU+N0HiuY0/gUwpwlcsnEooVcwAc4w9QtXdRVyltbFaOCEIooGAkKgIdMQODJrjZos1GPUZEogySJEQNIhoKMguQcYxvVatjiNStGBQBkaQDpAGyELBdkORj2wUBoBeVtQiZWdI2I+AAVz4TLZXTNrFhvHiqPMTItYoSK1ybmJRcnaquq6xrn+rNsxQfykLR4y8bDV5W6hLxNvGqjm7vBGWkuaYsiwpjyx/s9DDfKJQaDGUf4mVTDqECSKUoQ8kCKhMO3JZXzdDPFWzePw91RJFrOiHJIsxTLuVVGSTaE1Ui0BlZ1xERtldf9NNi8qsgKMmNUdUVi+dJk9RLKm6yvN/U2RUJSBinDsIAJMyRYC2GSQmQ2i6ZjWDpu1Y0Bb/0mB3I9wl2lrkmKmznsrdOtCkRCRyMTxGQliAgt1habCdlyw8qSC4DBRMLoCrey/aW0rVbNLS5KkWx4M8tbl2GQF9eZcLuYwQoSYUQU8VJIlGQAspnh9vp42RF3C5Sor/e7sSCBd+gUb65BlqHyIqJFkl47hUxF4piQHGBYGdFSOCrTwags00JICtNQRgggJAhilP3fkcSyTEWwLTRElJEHGaQxKinqBqrXVUV0qJEimekzQIsOHKNaLcvg/P729nZfO+PTzRZ/AADoX7ZoYVJsRSkyfgdllVsL00qGsyIiWljhLMcg0XQQ/hHnWgsVZ0ypnjctp8J2yDqJVJV8Bjk8LML4yN1/gYpdqJMLpKuPiQHeUHwuxX2invLrPGlpd6s5E7F3NEJ+wXaIfwmZPpN0oq6pzuj1WmxC4X7t0SlhUg1ffzcSGAMe3V4frdfhBsago2hthMu66YwR5nDlMejwoqjbmjUedwOqY9CQbVVWVNVt1/E4zFIfS5garzqWIuIhlxPqsoRh1EqTq8uaFIsMdKeJrsxM6GQrnaxYRochCLOMIDE0wwclISiFJ4RbQm2KiJqVSYZds1V1QriqUNuN9SS9vu2/xQVYJAkCYOiAGGHpYIBBtBBEiJYjUigq8FCUw3BCuBe1TLZS1vkTE0JUqSpkMjQ6k+1PcL6OLSRNDBq5kE4uUO6EGRwwwVCIpVkECE8JR2leDkp0SEbsaCGP2Mi60t1wpfcPf1jHczI/2OV5F3R5DpG3OYgAGs4GjxbmLc3Pu5PCioV8mNckQe/wKbzsw0qLRt6NJvKtQY7BK2ZuoefJ+3957GrWk75nJZg29KgvyoPFWS98MP36SQG8e2oJZAEMQMgGWRheAWZdP82HZfn37FgV3tW7MjTv3sI3Dl6C7WDDqeMzQMmQkMdTkJPf5cnZ/NoXZ4837OE/35cuda6c+TXy8Pquzpe3lX/9zbXLp7cWl+d9MuX8htKzq4zPtuf+eOAiveWJoy8+v2tlz77O7R81bD01u+9Y+7npu+uK5EMXDhXN75AfmdJLrevo6ptZ3Ln5+9c3fteHTi/z0pd/qD659plra5fuXbfhwnMPvTXniO+lvbkLChbAht1vX/lzy8I/9sjlf507OOvvmt+Vvqd27a8oaTx8mP3yw8cPzn3v06nTnXeOlzInp+0Hr361+ugJUe6dcSLSvWZ9mXjs5NPCzhfmRJZX1sw6tu5q58YPpp5/8+IBVLaJClzq2tHcu/pn9ZUS44H0mZr297ur1j/78c5v9wR+m9FWdmRbM1+6aWZ/Gf8Bds5nt2wRAAA=",
+			Authorization:"Bearer v^1.1#i^1#r^0#p^3#f^0#I^3#t^H4sIAAAAAAAAAOVXe2wURRjvtb0SAkVNDG/NuRiikL2b3b29u91wZ64PbIW2J9c2PDQwuzvbruztXnZ2266KOauiWENJrJoYom2MxsSgwYQgoiEoRANRFP1HCCSCD4IISCQgIcbZ64NridAHiU28fzbzzff6fd/vm5sBubKpizbWbLxU7ptS3JsDuWKfj5kGppb5F88oKZ7rLwIFCr7e3L250s6SU0swzOhZcQXCWdPAKNCR0Q0s5oVxyrEM0YRYw6IBMwiLtiymk3XLRTYIxKxl2qZs6lSgtipOKVBCERSBYSDwapQHRGoM+mw04xSLGBZIYaRGo2EWsN4+xg6qNbANDZvsAyZGA4Zm2UYQFQEv8lxQ4LnVVKAZWVgzDaISBFQin66Yt7UKcr1xqhBjZNnECZWoTS5NNyRrq6rrG5eECnwlBuqQtqHt4OGrSlNBgWaoO+jGYXBeW0w7sowwpkKJ/gjDnYrJwWTGkX6+1LzAASQIUgwKAh9G7C0p5VLTykD7xnl4Ek2h1byqiAxbs92bVZRUQ3oMyfbAqp64qK0KeJ+HHahrqoasOFVdkVzVlK5eQQXSqZRltmkKUjykDMfFABfhSLI2wqSEyForW27WNnGrmR2I1u9yoNYjwlWahqJ5lcOBetOuQCR1NLxAEZEvKBBRajAarKRqe2kV6gmDhQyHV3ud7W+lY7caXnNRhlQjkF/evA2DvLjGhFvFDARjghSNClwUIBBRIoXM8GZ9vOxIeA1KplIhLxckQZfOQGs9srM6lBEtk/I6GWRpisjxKsvFVEQrEUGlw4Kq0hKvRGhGRQggJEmyEPvfkcS2LU1ybDRElJEbeaRxKi2bWZQydU12qZEq+dNngBYdOE612nZWDIXa29uD7VzQtFpCLABMaGXd8rTcijKQGtLVbq5Ma3nOyohYYU203SzJpoPwjwQ3WqgEZykpaNluheOSdRrpOvkMcnhYhomR0n+Bij2okwukZ4+JA5jVgh7Fg7KZCZmQjLQnWpvPODAapZDkuCS+gqyghaBiGro7ersWh1C433p0Rph0I9g/jQTGQERv1kcbdbiDMdhoRhvhsmm5Y4Q53HgMNlCWTcewxxNuwHQMFqqjq5que+M6noAF5mNJ04C6a2syHgo5oSlLZrO1yuSasmbNJge620xX5k/odCudrlhJRxgQiXGSwtEcDCtSWIlMCLeC2jQZrdUmGXbD0fUJ4apCbdf3k8z62/8tLhBDiiQBjmblaIwOsxyiyUUf0WpU4QUJMrIaYSaEu65lsrWyPpScEKJKXSMnQ6M72f4Ea0xsI2Vi0MiFdHKB8k6YwQMmzPMxOoYA4SnhKA3VsELzKoqNFvIIQcGV7rorfWj4wzpRlP8xnb5PQadvF3mbgyigmcXg/rKSptKS6RTWbBTE0FAksyOoQTWItRaDvBstFFyP3CzUrOIy35p5px+4WvCk730UzB561E8tYaYVvPDB/Gs7fua2WeVMDDAsS8Ly5B4LFlzbLWVmlt55/A3+jh/ngi2f7fh6J/PQD92nNm45AMqHlHw+f1Fpp6/oroorP3/+zZqll5XX+ON986W+TRtSSkVPZm+7PX3fjtm7F74K3eZFlx5Hh7v+nPZ73ekmfOSZhb6XS+Rvr3z3YNcscOLJOfufvnA0xR8++O7mvw6s+2Xunv33PPteA3iqayt/Yt4edPLqot3bG3I7j5pTjpzdsTUllIBA0+a+c68H/LmqUrnHP/Pkxb9X/bZpceTMlnV/1Bw6ffvFpr3Pw86muovnF1x4Mw6SNWc6L//ae/J9cZ+/nO2aeaxFevGVLlX/6J14+olcatuyltSJS9u7/RL4/rndL731066u8CP3uR/3nPN9YnHbPji7dcqXx+YsmNG97ODdX3y1ZuGHM87neuYHD82e80L1/r4N/W38Bwxr/q1sEQAA",
+			"Accept":"application/json",
 			"Content-Type": "application/json",
 			"X-EBAY-C-ENDUSERCTX": "contextualLocation=country=<2_character_country_code>,zip=<zip_code>"
 		}
@@ -47,8 +48,9 @@ module.exports = function() {
 		});
 	});
 
+
 	//initiate checkout session
-	function initiateCheckOutSession(){
+	function initiateCheckOutSession(itemId, quantity){
 		options.url = "https://api.sandbox.ebay.com/buy/order/v1/checkout_session/initiate";
 		let requestBody = {
 			"shippingAddress": {
@@ -60,12 +62,20 @@ module.exports = function() {
 				"recipient": "Frank Smith",
 				"phoneNumber": "617 555 1212"
 			}
-			,"lineItemInputs": [{"quantity": 1, "itemId": "<item_id>"}]
+			,"lineItemInputs": [{"quantity": quantity, "itemId": itemId}]
 		}
-		request.post(options, {form:{key:'value'}}, (error, response, body)=>{
+		request.post(options, requestBody, (error, response, body)=>{
+			if(error){
+				throw error;
+			}
 			console.log(body);
 		});
 	}
+	routes.get("/test/:id", (req, res)=>{
+		console.log("test route runs");
+		initiateCheckOutSession(req.params.id);
+		res.status(200);
+	});
 
 	return routes;
 }
