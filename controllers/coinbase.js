f17055949ece0f0cfbc3490c167b742873a8a60f
Vitali Vorobyev <vvitali26@gmail.com>
2018-01-23 17:29:00 -0500

    get transactions

--- controllers/coinbase.js
+++ controllers/coinbase.js
@@ -13,8 +13,7 @@ module.exports = function() {
 			//it looks strange, but we have to keep it before we coinbase will activate API-key
 			if (err){
 				console.error("Error:" + err.message);
-				err.message == "API Key disabled" && console.log("\x1b[32mEverything is fine, we have to wait 48 hours since 1/18/18 for API key activation\x1b[0m");
-				res.send("API disabled: test passed!");
+				res.send(err.message);
 			}else{
 				account.createAddress(null, function(err, rObject) {					
 
@@ -27,25 +26,42 @@ module.exports = function() {
 
 	routes.get("/test", (req,res)=>{
 		
-		// const parametrs = {
-		// 	"amount": "10.00", 
-		// 	"currency": "USD", 
-		// 	"name": "Order #123",
-		// 	"description": "Sample order",
-		// 	"metadata": {
-		// 		"customer_id": "id_1005",
-		// 		"customer_name": "Satoshi Nakamoto"}
-		// 	}
+		/*
+		const parametrs = {
+			"amount": "10.00", 
+			"currency": "USD", 
+			"name": "Order #123",
+			"description": "Sample order",
+			"metadata": {
+				"customer_id": "id_1005",
+				"customer_name": "Satoshi Nakamoto"
+				}
+			}
 
-		// 	// client.createOrder(parametrs, function(error, order) {
-		// 	// 	if(error){
-		// 	// 		throw(error);
-		// 	// 	}
-		// 	// 	console.log(order);
-		// 	// });
-
-
+		client.createOrder(parametrs, function(error, order) {
+				if(error){
+					throw(error);
+				}
+				console.log(order);
 		});
+		*/
+
+		client.getAccount('primary', function(err, account) {
+			//it looks strange, but we have to keep it before we coinbase will activate API-key
+			if (err){
+				console.error("Error:" + err.message);
+				res.send(err.message);
+			}else{				
+				account.getTransactions(null,function(err, txs) {
+					txs.map((data)=>{
+						console.log(data.amount)
+
+					});
+					res.send(txs);
+				});
+			}
+		});
+	});
 
 	return routes;
 }
