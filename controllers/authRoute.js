a4ee1b3c5011ea8b768a466fbd1a92e9527fa88d
71emj <tim.jeng@gmail.com>
2018-01-22 21:48:21 -0500

    add lib/CRUD.js as helper in user collection querying

--- controllers/authRoute.js
+++ controllers/authRoute.js
@@ -1,8 +1,8 @@
 const DEBUG = true;
 
 const Passport = require("../config/jwt.js");
-const Users = require("../models").User;
 const authRoute = require("express").Router();
+const CRUD = require("../lib/CRUD");
 const hash = require("../lib/encryptor.js");
 const signToken = require("../lib/signToken.js");
 const mail = require("../lib/sendgrid.js");
@@ -10,89 +10,91 @@ const _ = require("lodash");
 
 module.exports = function() {
 
-	authRoute.post("/login", function(req, res) {
-		DEBUG && console.log(req.body);
-		const { username, password, email } = req.body;
-		const searchField = email ? { email } : { username };
+   authRoute.post("/login", function(req, res) {
+      DEBUG && console.log(req.body);
+      const { username, password, email } = req.body;
+      const searchField = email ? { email } : { username };
 
-		Users.findOne(searchField).then(user => {
-			if (!user) {
-				return res.status(401).json({ message: "no such user found" });
-			}
+      CRUD
+         .read(searchField)
+         .then(user => {
+            if (!user) {
+               return res.status(401).json({ message: "no such user found" });
+            }
 
-			hash
-				.compare(password, user)
-				.then(isMatched => {
-					if (isMatched) {
-						signToken(req, user, 5 * 60).then(() => {
-							DEBUG && console.log("this is fresh air", req.sessionID);
+            hash
+               .compare(password, user)
+               .then(isMatched => {
+                  if (isMatched) {
+                     signToken(req, user, 5 * 60).then(() => {
+                        DEBUG && console.log("this is fresh air", req.sessionID);
 
-							res.status(200).json({ message: "ok", token: req.session.token });
-						}).catch(DEBUG && console.log.bind(console)); // sign token with private key && store in session
-					} else {
-						res.status(401).json({ message: "passwords did not match" });
-					}
-				})
-				.catch(err => {
-					DEBUG && console.log(err);
-					res
-						.status(500)
-						.send(
-							"Internval Server Error. Please note that our engineer is working hard to recover it."
-						);
-				});
-		});
-	});
+                        res.status(200).json({ message: "ok", token: req.session.token });
+                     }).catch(DEBUG && console.log.bind(console)); // sign token with private key && store in session
+                  } else {
+                     res.status(401).json({ message: "passwords did not match" });
+                  }
+               })
+               .catch(err => {
+                  DEBUG && console.log(err);
+                  res
+                     .status(500)
+                     .send(
+                        "Internval Server Error. Please note that our engineer is working hard to recover it."
+                     );
+               });
+         });
+   });
 
-	authRoute.post("/register", function(req, res) {
-		DEBUG && console.log(req.body);
-		const { username, password, email } = req.body;
-		
-		hash
-			.create(password, username)
-			.then(({ salt, hash, publickey }) => {
-				const userData = {
-					username,
-					email,
-					salt,
-					publickey,
-					password: hash
-				};
-				return Users.create(userData);
-			})
-			.then(user => {
-				// timeout should be passed from a config.json which stores lots of stuff
-				signToken(req, user, 5 * 60, req).then(token => {
-					DEBUG && console.log("djfadjfla");
-					DEBUG && console.log(req.sessionID);
-					console.log(req.hostname);
-					console.log(req.originalUrl);
-					//////////////////////
-					mail(req.hostname, user, token);
-					/////////////////////
+   authRoute.post("/register", function(req, res) {
+      DEBUG && console.log(req.body);
+      const { username, password, email } = req.body;
 
-					res.status(200).json({ message: "ok", token: req.session.token });
-				}).catch(DEBUG && console.log.bind(console));
-			})
-			.catch(err => {
-				DEBUG && console.log("is this where the null is? %s", err);
-				res.status(401).send(err); // process mongo related error
-			});
-	});
+      hash
+         .create(password, username)
+         .then(({ salt, hash, publickey }) => {
+            return CRUD.create({
+               username,
+               email,
+               salt,
+               publickey,
+               password: hash
+            });
+         })
+         .then(user => {
+            // timeout should be passed from a config.json which stores lots of stuff
+            signToken(req, user, 5 * 60).then(refId => {
+               DEBUG && console.log("djfadjfla");
+               DEBUG && console.log(req.sessionID);
+               console.log(req.hostname);
+               console.log(req.originalUrl);
 
-	authRoute.get("/user", Passport.authenticate("jwt", { session: false }), function(
-		req,
-		res
-	) {
-		// not getting get request from react
-		DEBUG && console.log("======================================");
-		DEBUG && console.log(req.sessionID);
-		DEBUG && console.log(req.user);
-		const { user, session } = req;
-		const userInfo = session[user._id];
-	
-		res.status(200).json(userInfo);
-	});
+               //////////////////////
+               mail(req.hostname, user, refId);
+               /////////////////////
 
-	return authRoute;
+               res.status(200).json({ message: "ok", token: req.session.token });
+            }).catch(DEBUG && console.log.bind(console));
+         })
+         .catch(err => {
+            DEBUG && console.log("is this where the null is? %s", err);
+            res.status(401).send(err); // process mongo related error
+         });
+   });
+
+   authRoute.get("/user", Passport.authenticate("jwt", { session: false }), function(
+      req,
+      res
+   ) {
+      // not getting get request from react
+      DEBUG && console.log("======================================");
+      DEBUG && console.log(req.sessionID);
+      DEBUG && console.log(req.user);
+      const { user, session } = req;
+      const userInfo = session[user._id];
+
+      res.status(200).json(userInfo);
+   });
+
+   return authRoute;
 };
--- controllers/infoUpdate.js
+++ controllers/infoUpdate.js
@@ -1,31 +1,68 @@
 const infoRoute = require("express").Router();
-const Users = require("../models").User;
+const Passport = require("../config/jwt.js");
+const CRUD = require("../lib/CRUD");
+const signToken = require("../lib/signToken");
+const _ = require("lodash");
 const { authUser: memoryStore } = require("../config/config");
 
 module.exports = function() {
+   // infoRoute.all("/user/*", Passport.authenticate("jwt", { session: false }));
 
-	infoRoute.post("/forgotPass", function(req, res) {
-		
-		// send email
-		
+   infoRoute.post("/user/forgotPass", function(req, res) {
+      // send email
+   });
 
+   infoRoute.get("/user/verification", function(req, res, next) {
+      console.log("this is verification");
+      console.log(req.query["t"]);
+      const refId = req.query["t"];
+      const user = memoryStore[refId];
 
-	});
+      if (user) {
+         return CRUD
+            .update(user._id, { emailverified: true })
+            .then(data => {
+               console.log("this is data ", data);
+               return signToken(req, data, 5 * 60);
+            })
+            .then(refId => {
+            	console.log(req.session[memoryStore[refId]._id]);
+               res.status(200).json({
+                  message: "awesome",
+                  newRef: refId
+               });
+            })
+            .catch(console.log.bind(console));
+      }
 
-	// put
-	infoRoute.get("/user/verification", function(req, res) {
-		console.log(req.query["t"]);
-		res.status(200).send("awesome");
-	});
+      let isInSessHistory = false;
 
+      _.forIn(memoryStore, function(val, key) {
+         if (_.has(val.history, refId)) {
+            console.log("here");
+            isInSessHistory = memoryStore[key];
+         }
+      });
 
+      console.log("yayayayay");
 
+      if (isInSessHistory) {
+         return CRUD.update(isInSessHistory._id, { emailverified: true }).then(data => {
+            console.log("this is data ", data);
+            res.status(200).send("awesome");
+         });
+      }
 
+      res.status(401).send("requested link expired");
+   });
 
+   // two ways to trigger email
+   // auth - register
+   // before checkout
+   // but if user session auth changed
+   // now memorystore stores past session auth ref
+   // so if the user token doesn't match --- look for past
+   // session ref
 
-
-
-
-
-	return infoRoute;
-}
+   return infoRoute;
+};
