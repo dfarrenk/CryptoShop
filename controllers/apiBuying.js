e7f5698c549a36805eec671dc6a2dd586d7d73ec
Vitali Vorobyev <vvitali26@gmail.com>
2018-01-25 14:52:30 -0500

    0.2

--- controllers/apiBuying.js
+++ controllers/apiBuying.js
@@ -1,28 +1,26 @@
 const routes = require("express").Router();
 console.log("apiBying controller: \x1b[32mloaded!\x1b[0m");
-
 const eBay = require("../lib/eBay.js");
 const coinbase = require("../lib/coinbase.js")();
+const CRUD = require("../lib/CRUD.js")
 module.exports = function() {
 	
 	routes.post("/buyItem/", (req, res)=>{
 		console.log("buyItem route fires!");
+		let ObjectToPush = {
+			currency: "USD",
+			amountRecieved: 10,
+			btcAddress:req.body.btcAddress,
+			mailAddress: req.body.mailAddress,
+			ebayId:req.body.ebayId
+		}
+		res.status(200).send(CRUD.updatePush("5a676d898bc3332d3047e5fa", ObjectToPush));
 		
-		request(options, function (error, response, body) {
-			if (error) {
-				console.log(error);
-			} // Print the error if one occurred
-			//console.log('statusCode:', response && response.statusCode);  
-			res.status(200).send(body) ;
-			
-		});
 	}); 
 
 	routes.get("/test", (req, res)=>{
 		let temp =null;
-
 		coinbase.getAddress((address)=>{
-
 			res.send(address)
 		});
 	})
