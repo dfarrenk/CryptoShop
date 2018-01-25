d2e0af6abc22065f3238454a0f0cabfe8e893517
Vitali Vorobyev <vvitali26@gmail.com>
2018-01-25 13:48:53 -0500

    ebay controller, coinbase controller moved to lib, new apiController created

--- controllers/apiBuying.js
+++ controllers/apiBuying.js
@@ -1,12 +1,29 @@
 const routes = require("express").Router();
 console.log("apiBying controller: \x1b[32mloaded!\x1b[0m");
 
+const eBay = require("../lib/eBay.js");
+const coinbase = require("../lib/coinbase.js")();
 module.exports = function() {
 	
-
-	routes.get("/test", (req,res)=>{
+	routes.post("/buyItem/", (req, res)=>{
+		console.log("buyItem route fires!");
 		
-	});
+		request(options, function (error, response, body) {
+			if (error) {
+				console.log(error);
+			} // Print the error if one occurred
+			//console.log('statusCode:', response && response.statusCode);  
+			res.status(200).send(body) ;
+			
+		});
+	}); 
+
+	routes.get("/test", (req, res)=>{
+		let temp =null;
+
+		temp = coinbase.getAddress();
+		res.status(200).send(temp)
+	})
 
 	return routes;
 }
