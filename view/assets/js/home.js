3e2b18fb6a424053cf798d149aea9bf029154735
Shaun Bens <bensshaun@gmail.com>
2018-01-26 17:21:01 +0000

    sign out functionality

--- view/assets/js/home.js
+++ view/assets/js/home.js
@@ -1,18 +1,25 @@
 $(function() {
 
-  const loggedInUser = ({
-    username: username,
-    password: password,
-    email: email
-  });
+  // const loggedInUser = ({
+  //   username: username,
+  //   password: password,
+  //   email: email
+  // });
   let username;
   let email;
   let password;
 
-  $("#logInStatic").click(function() {
+  $("#logInStatic").click(function(e) {
+    e.preventDefault();
     username = $("#loginEmail").val().trim();
     email = $("#loginEmail").val().trim();
     password = $("#loginPass").val().trim();
+
+    const loggedInUser = ({
+      username: username,
+      password: password,
+      email: email
+    });
 
     $.post({
       url: "/login",
@@ -38,6 +45,34 @@ $(function() {
     }).catch(console.log.bind(console));
   }
 
+  $("#registerStatic").click(function(e) {
+    username = $("#signUpUsername").val().trim();
+    email = $("#signUpEmail").val().trim();
+    password = $("#signUpPass").val().trim();
 
+    const loggedInUser = ({
+      username: username,
+      password: password,
+      email: email
+    });
+
+    $.post({
+      url: "/register",
+      data: loggedInUser
+    }).then((response) => {
+      console.log(response);
+      tokenSession(response.token).then(getMainPage);
+    }).catch(console.log.bind(console));
+  });
+
+  $("#signOutStatic").click(function(e) {
+    e.preventDefault();
+
+    $.post({
+      url: "/logout"
+    }).then(function(res) {
+      console.log(res);
+    });
+  });
 
 }); // Document Ready End
