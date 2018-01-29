0ddef3c3cd1c390bb468c5249a04ae108576fb2f
71emj <tim.jeng@gmail.com>
2018-01-29 14:49:46 -0500

    refactor server.js --> move some routes to controllers/htmlRoute

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var bodyParser = require("body-parser");
 var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
-var db = require("./models");
+// var db = require("./models");
 var exphbs = require("express-handlebars");
 
-var blockexplorer = require("blockchain.info/blockexplorer"); //another way to get a TXID confirmation
+// var blockexplorer = require("blockchain.info/blockexplorer"); //another way to get a TXID confirmation
 
 const _ = require("lodash");
 const ExpSess = require("express-session");
@@ -22,21 +22,23 @@ const {
 } = require("./config/config.js");
 const { port: PORT, httpsPort: PORTs, mongoURL } = serConf;
 
+
+// http/https setup
 const http = require("http");
 const server = http.createServer(app);
-
-// https setup
 const https = require("https");
 const certificate = httpsConf;
 const server_s = https.createServer(certificate, app);
 
-//Handlebars
-app.use(express.static("public"));
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+// static
 const staticPath = Join(__dirname, "view");
+app.use(express.static("public"));
 app.use(express.static(staticPath));
 app.use("*", express.static(staticPath));
+
+// handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Connect to the Mongo DB
 var MONGODB_URI = mongoURL;
@@ -48,87 +50,19 @@ app.use(ExpSess(sessConf));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set("forceSSLOptions", fsslConf);
+app.use(ForceSSL);
 
-// app.use(ForceSSL);
-
-app.all("*", require("./controllers")); // all router
+// all routers
+app.all("*", require("./controllers")); 
 
 server.listen(PORT, function(err) {
    console.log("Server started at: %s", server.address().port);
 });
 
 server_s.listen(PORTs, function(err) {
-   // trigger garbage collector on server starts
    memoryStore.garbageCollector();
-
    console.log("Https server running on port %s", server_s.address().port);
    console.log("\x1b[32mI'm ready to serve you, my master!\x1b[0m")
 });
 
-app.get("/", (req, res) => {
-   console.log("/");
-   res.status(200).sendFile(Join(__dirname, "./view/homepage.html"));
-});
-
-app.get("/txid/:TXID", (req, res) => {
-   res.send(blockexplorer.getTx(req.params.TXID));
-});
-
-/*
-
-@jwt-testing bench
-
-@use-Users object to test login
-
-*/
-
-app.get("/login", (req, res) => {
-   // console.log(req.session);
-   console.log("get");
-   // res.clearCookie("jwt-token");
-   console.log(req.path);
-   console.log(req.session.id);
-   // res.send("/login");
-   res.sendFile(Join(__dirname, "./cryptoshopreact/public/login.html"));
-   // res.sendFile(Join(__dirname, "./cryptoshopreact/public/index.html"));
-});
-
-app.post("/api/user", function(req, res) {
-   console.log(req);
-   //var query = {'username':req.user.username};
-   //var query - ('_id': req.user._id);
-   req.newData.username = req.user.username;
-   // req.newData.field = req.user.field;
-   db.User.findOneAndUpdate(query, req.newData, function(err, doc) {
-      if (err) return res.send(500, { error: err });
-      return res.send("succesfully saved");
-   });
-});
-
-//Test route for getting Users from MongoDB. It will pull all user documents from the 'users' collection in the 'crypto' database.
-app.get("/api/user", function(req, res) {
-   db.User.find({})
-   .then(function(dbUser) {
-      res.json(dbUser);
-   })
-   .catch(function(err) {
-      res.json(err);
-   });
-});
-
-//Test route to add a User
-app.get("/api/user/testUser", function(req, res) {
-   console.log("herer");
-   let testUser = { name: "testUser", password: "1234", email: "testUser@gmail.com" };
-   db.User.create(testUser).then(function(res) {
-      console.log(`User inserted`);
-      res.status(200).json(res);
-   });
-});
-
-app.get("/search/:id", function(req, res) {
-   let searchTerm = req.params.id;
-   res.status(200).send("/searchPage.html?item=" + searchTerm);
-});
-
-module.exports = app;
+// module.exports = app;
