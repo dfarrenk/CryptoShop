5bd397e4aacc8e0ec7d392da6e15051351fb1b56
71emj <tim.jeng@gmail.com>
2018-01-31 23:07:44 -0500

    handlebars inplace, without error modal

--- controllers/htmlRoute.js
+++ controllers/htmlRoute.js
@@ -1,11 +1,10 @@
 const DEBUG = false;
-const Join = require("path").join;
-const _ = require("lodash");
 const Auth = require("../lib/authcallback.js")("manual");
+const Handlebars = require("handlebars");
 const ServErr = require("../util/servError.js");
-const Uid = require("uid-safe").sync;
 const htmlRoute = require("express").Router();
 const { memoryStore } = require("../config/config.js");
+const place = require("../lib/json/states.json");
 
 require("../util/errorHandler")();
 
@@ -26,7 +25,9 @@ module.exports = function() {
       console.log(res.locals.error);
       return res.status(302).redirect("/search");
     }
-    res.status(200).render("profilepage")
+		const { user, session } = req;
+		const userInfo = session[user._id];
+    res.status(200).render("profilepage", { user: userInfo, place });
   });
 
   return htmlRoute;
@@ -40,7 +41,6 @@ console.log("eBay controller: \x1b[32mloaded!\x1b[0m");
 // });
 
 
-
 // htmlRoute.post("/api/user", function(req, res) {
 //    console.log(req);
 //    //var query = {'username':req.user.username};
@@ -50,26 +50,5 @@ console.log("eBay controller: \x1b[32mloaded!\x1b[0m");
 //    db.User.findOneAndUpdate(query, req.newData, function(err, doc) {
 //       if (err) return res.send(500, { error: err });
 //       return res.send("succesfully saved");
-//    });
-// });
-
-//Test route for getting Users from MongoDB. It will pull all user documents from the 'users' collection in the 'crypto' database.
-// htmlRoute.get("/api/user", function(req, res) {
-//    db.User.find({})
-//    .then(function(dbUser) {
-//       res.json(dbUser);
-//    })
-//    .catch(function(err) {
-//       res.json(err);
-//    });
-// });
-
-// //Test route to add a User
-// htmlRoute.get("/api/user/testUser", function(req, res) {
-//    console.log("herer");
-//    let testUser = { name: "testUser", password: "1234", email: "testUser@gmail.com" };
-//    db.User.create(testUser).then(function(res) {
-//       console.log(`User inserted`);
-//       res.status(200).json(res);
 //    });
 // });
