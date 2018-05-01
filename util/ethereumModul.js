5a1c34f10a06df74270d67e11a8c228d730c66e6
Vvitali <vvitali26@gmail.com>
2018-04-30 21:11:49 -0400

    config file for ethereum module

--- util/ethereumModul.js
+++ util/ethereumModul.js
@@ -1,5 +1,7 @@
 var Web3 = require('web3');
 var util = require('ethereumjs-util');
-var tx = require('ethereumjs-tx');
-var lightwallet = require('eth-lightwallet');
-var txutils = lightwallet.txutils;
+var tx = require('ethereumjs-tx'); 
+var {address, key}  = require("../config/ethereum.js"); 
+
+//connection to eth-blockchain node
+//var web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/'));
